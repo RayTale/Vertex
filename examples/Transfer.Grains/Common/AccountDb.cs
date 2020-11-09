16b452ab8d07d3fcd8e57952fed9f21fb50fa4db
洁 费 <feijie89@qq.com>
2020-11-09 18:05:13 +0800

    添加db实现代码

--- examples/Transfer.Grains/Common/AccountDb.cs
+++ examples/Transfer.Grains/Common/AccountDb.cs
@@ -1,7 +1,12 @@
 ﻿using System.Threading.Tasks;
+using AutoMapper;
+using IdGen;
+using Microsoft.EntityFrameworkCore;
 using Orleans;
 using Transfer.Grains.Events;
+using Transfer.Grains.Snapshot;
 using Transfer.IGrains.Common;
+using Transfer.Repository;
 using Vertex.Abstractions.Actor;
 using Vertex.Runtime.Actor;
 using Vertex.Storage.Linq2db.Core;
@@ -15,36 +20,55 @@ namespace Transfer.Grains.Common
     public sealed class AccountDb : FlowActor<long>, IAccountDb
     {
         private readonly IGrainFactory grainFactory;
+        private readonly AccountSnapshotHandler accountSnapshotHandler;
+        private readonly IMapper mapper;
 
-        public AccountDb(IGrainFactory grainFactory)
+        public AccountDb(IGrainFactory grainFactory, AccountSnapshotHandler accountSnapshotHandler)
         {
             this.grainFactory = grainFactory;
+            this.accountSnapshotHandler = accountSnapshotHandler;
         }
 
         public override IVertexActor Vertex => this.grainFactory.GetGrain<IAccount>(this.ActorId);
 
-        public Task EventHandle(TransferEvent evt, EventMeta eventBase)
+        public async Task EventHandle(TransferEvent evt, EventMeta eventBase)
         {
-            // Update database here
-            return Task.CompletedTask;
+            using (var db = new TransferDbContext())
+            {
+                var entity = await db.Accounts.FirstOrDefaultAsync(x => x.Id == this.ActorId);
+                this.accountSnapshotHandler.EntityHandle(entity, evt);
+                await db.SaveChangesAsync();
+            }
         }
 
-        public Task EventHandle(TopupEvent evt, EventMeta eventBase)
+        public async Task EventHandle(TopupEvent evt, EventMeta eventBase)
         {
-            // Update database here
-            return Task.CompletedTask;
+            using (var db = new TransferDbContext())
+            {
+                var entity = await db.Accounts.FirstOrDefaultAsync(x => x.Id == this.ActorId);
+                this.accountSnapshotHandler.EntityHandle(entity, evt);
+                await db.SaveChangesAsync();
+            }
         }
 
-        public Task EventHandle(TransferArrivedEvent evt, EventMeta eventBase)
+        public async Task EventHandle(TransferArrivedEvent evt, EventMeta eventBase)
         {
-            // Update database here
-            return Task.CompletedTask;
+            using (var db = new TransferDbContext())
+            {
+                var entity = await db.Accounts.FirstOrDefaultAsync(x => x.Id == this.ActorId);
+                this.accountSnapshotHandler.EntityHandle(entity, evt);
+                await db.SaveChangesAsync();
+            }
         }
 
-        public Task EventHandle(TransferRefundsEvent evt, EventMeta eventBase)
+        public async Task EventHandle(TransferRefundsEvent evt, EventMeta eventBase)
         {
-            // Update database here
-            return Task.CompletedTask;
+            using (var db = new TransferDbContext())
+            {
+                var entity = await db.Accounts.FirstOrDefaultAsync(x => x.Id == this.ActorId);
+                this.accountSnapshotHandler.EntityHandle(entity, evt);
+                await db.SaveChangesAsync();
+            }
         }
     }
 }
