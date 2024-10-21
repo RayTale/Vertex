b6c5a1bf193a36be7e36ac7f75f8a2a24c5def96
feijie <feijie89@qq.com>
2024-10-21 11:53:17 +0800

    🎉 feat(TransferRequest): 添加序列化支持和字段标识符到 TransferRequest 类

--- examples/Transfer.IGrains/DTx/TransferRequest.cs
+++ examples/Transfer.IGrains/DTx/TransferRequest.cs
@@ -1,13 +1,20 @@
-﻿namespace Transfer.IGrains.DTx
+﻿using Orleans;
+
+namespace Transfer.IGrains.DTx
 {
+    [GenerateSerializer]
     public class TransferRequest
     {
+        [Id(0)]
         public string Id { get; set; }
 
+        [Id(1)]
         public long FromId { get; set; }
 
+        [Id(2)]
         public long ToId { get; set; }
 
+        [Id(3)]
         public decimal Amount { get; set; }
     }
 }
