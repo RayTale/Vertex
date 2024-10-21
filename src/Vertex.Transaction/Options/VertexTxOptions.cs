1da097409f8f450df3f8cdfdd68d2fc943a292b9
feijie <feijie89@qq.com>
2024-10-21 12:00:28 +0800

    🎉 feat(options): 添加序列化生成器和属性ID到交易选项类

--- src/Vertex.Transaction/Options/VertexTxOptions.cs
+++ src/Vertex.Transaction/Options/VertexTxOptions.cs
@@ -1,10 +1,14 @@
-﻿namespace Vertex.Transaction.Options
+﻿using Orleans;
+
+namespace Vertex.Transaction.Options
 {
+    [GenerateSerializer]
     public class VertexTxOptions
     {
         /// <summary>
         /// Transaction timeout time(default is 30s)
         /// </summary>
+        [Id(0)]
         public int TxSecondsTimeout { get; set; } = 30;
     }
 }
